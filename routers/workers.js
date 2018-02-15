525447df48224247d638f265fb4280adf5c7ba3e
Stanislav Okunev <stanok3694@yandex.ru>
2018-02-15 19:18:22 +0300

    tech: draft for worker create func (need to delete empty worker from db)

--- routers/workers.js
+++ routers/workers.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const workerModel = require('../dal/models/worker.model');
 const prepareWorkersData = require('../middleware/prepareWorkersData');
+const checkResults = require('../middleware/checkResults');
 
 router.get('/', (req, res) => {
     res.send('You are in the workers section');
@@ -10,17 +11,31 @@ router.get('/', (req, res) => {
 
 router.get('/getAllWorkers', (req, res) => {
     // SO: need to think about rework that from callback to promise!
-    workerModel.find().lean().exec((err, workers) => {
-        if (err) return console.log(err);
+    workerModel
+        .find().lean().exec((err, workers) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
 
-        const results = prepareWorkersData(workers);
-        
-        if (results.length > 0) {
-            res.send(results);
-        } else {
-            res.send('You have no workers yet!');
-        }
-    });
+            const results = prepareWorkersData(workers);
+            res.send(checkResults(results));
+        });
+});
+
+router.post('/createWorker', (req, res) => {
+    console.log(req);
+    
+    // workerModel.create({
+
+    // }, (err, worker) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+
+    //     res.send(`Successfully created: ${worker}`);
+    // });
 });
 
 module.exports = router;
