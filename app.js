00468490907d436e608e74ec8d7a60e2ef4bd9cf
Stanislav Okunev <stanok3694@yandex.ru>
2018-02-19 03:35:30 +0300

    tech: fixed cors issue

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const entry = require('./routers/entry');
 
 // SO: need move localhost url to server.config.js
 mongoose.connect('mongodb://localhost:27017');
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -22,6 +29,6 @@ app.use((req, res, next) => {
     res.status(404).send('Sorry, cannot find anything by this route!');
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('Rest Api is on 3000 port!');
 });
